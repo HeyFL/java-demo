c7703f54ccc29c88f19ac133d96f4b66a405a633
qq5525501ew <361389383@qq.com>
2018-02-01 15:10:54 +0800

    null

--- src/main/java/com/example/test/demo/zookeeper/CuratorRecipesZookeeperLockTest.java
+++ src/main/java/com/example/test/demo/zookeeper/CuratorRecipesZookeeperLockTest.java
@@ -13,9 +13,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class CuratorZookeeperLockTest {
+public class CuratorRecipesZookeeperLockTest {
     static int i = 0;
-    private static String address = "172.16.30.72:2181";//master:2181,worker1:2181,worker2:2181
+    //private static String address = "172.16.30.72:2181";//master:2181,worker1:2181,worker2:2181
+    private static String address = "127.0.0.1:2182,127.0.0.1:2181,127.0.0.1:2183";//master:2181,worker1:2181,worker2:2181
     private static CuratorFramework client = CuratorFrameworkFactory.newClient(address, new ExponentialBackoffRetry(1000, 3));//静态  不需要close
     //创建锁，为不可重入锁，即是获锁后，不可以再次获取，这里不作例子，使用和重入锁类似
 //InterProcessSemaphoreMutex lock = new InterProcessSemaphoreMutex(client, lockPath);
@@ -28,7 +29,7 @@ public class CuratorZookeeperLockTest {
     private String PATH = "/zkLockRoot/lock_1";//根
     private InterProcessMutex lock;
 
-    public CuratorZookeeperLockTest() {
+    public CuratorRecipesZookeeperLockTest() {
         lock = new InterProcessMutex(client, PATH);
     }
 
@@ -37,7 +38,7 @@ public class CuratorZookeeperLockTest {
         for (int i = 0; i < 100; i++) {
             pool.submit(new Thread(() -> {
                 try {
-                    new CuratorZookeeperLockTest().test();
+                    new CuratorRecipesZookeeperLockTest().test();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
